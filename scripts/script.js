0f8ea468e1cfb7594877ac9d3df48950775d6903
Diego Alves <dihgalvao@gmail.com>
2022-06-27 20:03:13 -0300

    Version1

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+let page = document.querySelector(".page");
+let editButton = document.querySelector(".profile__edit-button");
+let popupCloseButton = document.querySelector(".popup__closebutton");
+let editPopUp = document.querySelector(".popup");
+let profileName = document.getElementById("name");
+let profileAbout = document.getElementById("aboutme");
+let popupName = document.querySelector(".popup__name");
+let popupExplore = document.querySelector(".popup__explore");
+
+editButton.addEventListener("click", handleShowPopUp);
+
+function handleShowPopUp() {
+  editPopUp.classList.add("popup_opened");
+  popupName.value = profileName.textContent;
+  popupExplore.value = profileAbout.textContent;
+}
+
+popupCloseButton.addEventListener("click", handleClosePopUp);
+
+function handleClosePopUp() {
+  editPopUp.classList.remove("popup_opened");
+}
+
+let formElement = document.querySelector(".popup__form");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = popupName.value;
+  profileAbout.textContent = popupExplore.value;
+  handleClosePopUp();
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
