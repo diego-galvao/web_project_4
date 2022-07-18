13686b79f3d3ca694eda337fa298dce68ab4804b
Diego Alves <dihgalvao@gmail.com>
2022-07-18 20:31:42 -0300

    Abrir imagem em popup

--- scripts/script.js
+++ scripts/script.js
@@ -25,38 +25,63 @@ const initialCards = [
   },
 ];
 
+// Criação de cards iniciais -----------------------------------------------------------------------
+
 const elements = document.querySelector(".elements");
 
-function createCard(card) {
+function saveCard(card) {
   const template = document.querySelector("template").content;
   const element = template.querySelector(".element").cloneNode(true);
 
   element.querySelector(".element__image").src = card.link;
   element.querySelector(".element__place").textContent = card.name;
 
-  elements.append(element);
+  elements.prepend(element);
+
+  const likeButton = element.querySelector(".element__like");
+  likeButton.addEventListener("click", () =>
+    likeButton.classList.toggle("element__like_active")
+  );
+
+  const trash = element.querySelector(".element__trash");
+  trash.addEventListener("click", () => trash.closest(".element").remove());
+
+  const cardImage = element.querySelector(".element__image");
+  cardImage.addEventListener("click", () => handleShowImagePopUp(card));
 }
 
-initialCards.forEach((card) => createCard(card));
+const modalImagePopUp = document.querySelector("#modal-image");
+
+function handleShowImagePopUp(card) {
+  const img = modalImagePopUp.querySelector(".popup__image");
+  const imgPlace = modalImagePopUp.querySelector(".popup__place");
+
+  img.src = card.link;
+  imgPlace.textContent = card.name;
+  modalImagePopUp.classList.add("popup__img-opened");
+}
+
+function handleCloseImagePopUp() {
+  modalImagePopUp.classList.remove("popup__img-opened");
+}
+
+const popupImageCloseButton = document.querySelector("#imagepopup_closebutton");
+popupImageCloseButton.addEventListener("click", handleCloseImagePopUp);
+
+initialCards.forEach((card) => saveCard(card));
 
 //----------------------------------------------------------------------------------
 
 const page = document.querySelector(".page");
-const editButton = document.querySelector(".profile__edit-button");
+
+// Classes do popup de edição  -----------------------------------------------------
+
 const popupCloseButton = document.querySelector(".popup__closebutton");
-const editPopUp = document.querySelector(".popup");
-const profileName = document.getElementById("name");
-const profileAbout = document.getElementById("aboutme");
-const popupName = document.querySelector(".popup__name");
-const popupExplore = document.querySelector(".popup__explore");
 
-const likeButtons = document.querySelectorAll(".element__like");
-likeButtons.forEach((button) =>
-  button.addEventListener("click", () =>
-    button.classList.toggle("element__like_active")
-  )
-);
+// Abertura e fechamento de popup ---------------------------------------------------
 
+const editButton = document.querySelector(".profile__edit-button");
+const editPopUp = document.querySelector("#edit-popup");
 editButton.addEventListener("click", handleShowPopUp);
 
 function handleShowPopUp() {
@@ -71,6 +96,13 @@ function handleClosePopUp() {
   editPopUp.classList.remove("popup_opened");
 }
 
+// Salvar dados popup de edição   ---------------------------------------------------
+
+const profileName = document.getElementById("name");
+const profileAbout = document.getElementById("aboutme");
+const popupName = document.querySelector("#fname");
+const popupExplore = document.querySelector("#fexplore");
+
 let formElement = document.querySelector(".popup__form");
 
 function updateProfile(evt) {
@@ -81,3 +113,39 @@ function updateProfile(evt) {
 }
 
 formElement.addEventListener("submit", updateProfile);
+
+// Abrir e fechar popup de adição de lugar ------------------------------------------------------------------
+const addPopUp = document.querySelector("#add-popup");
+
+const addButton = document.querySelector(".profile__add-button");
+const popupAddCloseButton = document.querySelector("#addpopup_closebutton");
+
+addButton.addEventListener("click", handleShowAddPopUp);
+
+function handleShowAddPopUp() {
+  addPopUp.classList.add("popup_opened");
+}
+
+popupAddCloseButton.addEventListener("click", handleCloseAddPopUp);
+
+function handleCloseAddPopUp() {
+  addPopUp.classList.remove("popup_opened");
+}
+
+//Adicionar card --------------------------------------------------------
+
+const popupTitle = document.querySelector("#ftitle");
+const popupLink = document.querySelector("#flink");
+
+let addElement = document.querySelector("#addpopup_form");
+
+function addCard(evt) {
+  evt.preventDefault();
+
+  const card = { link: popupLink.value, name: popupTitle.value };
+  saveCard(card);
+
+  handleCloseAddPopUp();
+}
+
+addElement.addEventListener("submit", addCard);
